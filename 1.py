77d485bd348c76779049112859c2c380c4ce0a04
Ansh Jha <ansh.jha0304@gmail.com>
2025-08-16 05:16:30 +0530

    update2

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+import os
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,3 +36,63 @@ def draw_bounding_box_on_image(image, ymin, xmin, ymax, xmax, color="red", thick
         (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
     draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=thickness, fill=color)
     #%%
+def dataset_to_numpy_util(training_dataset, validation_dataset, N):
+    batch_train_ds = training_dataset.unbatch().batch(N)
+
+    if tf.executing_eagerly():
+        for validation_digits, (validation_labels, validation_bboxes) in validation_dataset:
+            validation_digits = validation_digits.numpy()
+            validation_labels = validation_labels.numpy()
+            validation_bboxes = validation_bboxes.numpy()
+            break
+        for training_digits, (training_labels, training_bboxes) in training_dataset:
+            training_digits = training_digits.numpy()
+            training_labels = training_labels.numpy()
+            training_bboxes = training_bboxes.numpy()
+            break
+            
+    validation_labels = np.argmax(validation_labels, axis=1)
+    training_labels = np.argmax(training_labels, axis=1)
+    return (training_digits, training_labels, training_bboxes,
+            validation_digits, validation_labels, validation_bboxes)
+#%%
+MATPLOTLIB_FONT_DIR = os.path.join(os.path.dirname(plt. _file_), "mpl-data/fonts/ttf")
+def create_digits_from_local_fonts(n):
+    font_labels = []
+    img = PIL.Image.new('LA', (75*n, 75), color=(0,255))
+    font1 = PIL.ImageFont.truetype(os.path.join(MATPLOTLIB_FONT_DIR, 'DejaVuSansMono-Oblique.ttf'), 25)
+    font2 = PIL.ImageFont.truetype(os.path.join(MATPLOTLIB_FONT_DIR, 'STIXGeneral.ttf'), 25)
+    d = PIL.ImageDraw.Draw(img)
+
+    for i in range(n):
+        font_labels.append(i%10)
+        d.text((75*i+75, 0 if i < 10 else -4), str(i%10), fill=(255,255), font=font1 if i < 10 else font2)
+
+    font_digits = np.array(img.getdata(), np.float32)[:, 0] / 255.0
+    font_digits = np.reshape(np.stack(np.split(np.reshape(font_digits, [75, 75*n]), n, axis=1), axis=0), [n, 75, 75])
+    return font_digits, font_labels
+#%%
+def display_digits_with_boxes(digits, predictions, labels, pred_bboxes, bboxes, iou, title):
+    n = 10
+    indexes = np.random.choice(len(predictions), size=n)
+    n_digits = digits[indexes]
+    n_predictions = predictions[indexes]
+    n_labels = labels[indexes]
+
+    n_iou = []
+    if len(iou) > 0:
+        n_iou = iou[indexes]
+    
+    if len(pred_bboxes) > 0:
+        n_pred_bboxes = pred_bboxes[indexes]
+        
+    if len(bboxes) > 0:
+        n_bboxes = bboxes[indexes]
+
+    n_digits = n_digits * 255.0
+    n_digits = n_digits.reshape(n, 75, 75)
+
+    fig = plt.figure(figsize=(20,4))
+    plt.title(title)
+    plt.xticks([])
+    plt.yticks([])
