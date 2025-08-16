d9a3ad5fcfd12774d4793950fa15f6fb54f7c57a
Ansh Jha <ansh.jha0304@gmail.com>
2025-08-16 06:15:26 +0530

    update3

--- 1.py
+++ 1.py
@@ -96,3 +96,74 @@ def display_digits_with_boxes(digits, predictions, labels, pred_bboxes, bboxes, 
     plt.title(title)
     plt.xticks([])
     plt.yticks([])
+
+    for i in range(n):
+        for i in range(10):
+            ax = fig.add_subplot(1, 10, i + 1)
+    bboxes_to_plot = []
+    if (len(pred_bboxes) > i):
+        bboxes_to_plot.append(n_pred_bboxes[i])
+        
+    if (len(bboxes) > i):
+        bboxes_to_plot.append(n_bboxes[i])
+
+    img_to_draw = draw_bounding_boxes_on_image_array(image=n_digits[i],
+                                                     boxes=np.asarray(bboxes_to_plot),
+                                                     color=['red', 'green'],
+                                                     display_str_list=["True", "Pred"])
+
+    plt.xlabel(n_predictions[i])
+    plt.xticks([])
+    plt.yticks([])
+    
+    if n_predictions[i] != n_labels[i]:
+        ax.xaxis.label.set_color('red')
+
+    plt.imshow(img_to_draw)
+    iou_threshold = 0.5  # or any value you want
+    if len(iou) > i:
+        color = "black"
+        if (n_iou[i][0] < iou_threshold):
+            color = "red"
+        ax.text(0.2, -0.3, "iou: %s" %(n_iou[i][0]), color=color, transform=ax.transAxes)
+    
+#%%
+strategy = tf.distribute.get_strategy()
+strategy.num_replicas_in_sync
+#%%
+BATCH_SIZE = 64 * strategy.num_replicas_in_sync
+#%%
+def read_image_tfds(image, label):
+    xmin = tf.random.uniform([], 0, 48, dtype=tf.int32)
+    ymin = tf.random.uniform([], 0, 48, dtype=tf.int32)
+    image = tf.reshape(image, (28, 28, 1))
+    image = tf.image.pad_to_bounding_box(image, ymin, xmin, 75, 75)
+    image = tf.cast(image, tf.float32) / 255.0
+    xmin = tf.cast(xmin, tf.float32)
+    ymin = tf.cast(ymin, tf.float32)
+
+    xmax = (xmin + 28) / 75
+    ymax = (ymin + 28) / 75
+    xmin = xmin / 75
+    ymin = ymin / 75
+    
+    return image, (tf.one_hot(label, 10), [xmin, ymin, xmax, ymax])
+#%%
+def get_training_dataset():
+    with strategy.scope():
+        dataset = tfds.load("mnsit", split="train", as_supervised=True, try_gcs=True)
+        dataset = dataset.map(read_image_tfds, num_parallel_calls=16)
+        dataset = dataset.shuffle(5000, reshuffle_each_iteration=True)
+        dataset = dataset.repeat()
+        dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+        dataset = dataset.prefetch(-1)
+        return dataset
+#%%
+def get_validation_dataset():
+    with strategy.scope():
+        dataset = tfds.load("mnsit", split="train", as_supervised=True, try_gcs=True)
+        dataset = dataset.map(read_image_tfds, num_parallel_calls=16)
+        dataset = dataset.batch(10000, drop_remainder=True)
+        dataset = dataset.repeat()
+        return dataset
+#%%
